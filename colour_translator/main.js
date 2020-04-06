8903be403de96ceb910beb176a948f15822f487e
Robert King <robert.o.king@hotmail.co.uk>
2020-04-06 23:52:04 +0200

    Scripts to update hex and rgb values based on current rgb sliders

--- colour_translator/main.js
+++ colour_translator/main.js
@@ -1,21 +1,48 @@
-const box = document.getElementById('box')
+const box = document.getElementById('box');
 
-const sliders = document.querySelectorAll('.sliders input')
+const sliders = document.querySelectorAll('.sliders input');
 
-const red = document.getElementById('red')
-const green = document.getElementById('green')
-const blue = document.getElementById('blue')
+const red = document.getElementById('red');
+const green = document.getElementById('green');
+const blue = document.getElementById('blue');
+
+const hex = document.getElementById('hex');
+const rgb = document.getElementById('rgb');
+const hsl = document.getElementById('hsl');
+
 
 console.log(sliders);
 
 const updateBoxRGB = () => {
-  console.log("Test");
   box.style.backgroundColor = `rgb(${red.value}, ${green.value}, ${blue.value})`;
+};
+
+const updateHex = () => {
+  let hexRed = parseInt(red.value).toString(16).toUpperCase();
+  if (hexRed.length === 1) {
+    hexRed = "0" + hexRed;
+  }
+  let hexGreen = parseInt(green.value).toString(16).toUpperCase();
+  if (hexGreen.length === 1) {
+    hexGreen = "0" + hexGreen;
+  }
+  let hexBlue = parseInt(blue.value).toString(16).toUpperCase();
+  if (hexBlue.length === 1) {
+    hexBlue = "0" + hexBlue;
+  }
+
+  hex.value = `#${hexRed}${hexGreen}${hexBlue}`;
+};
+
+const updateRGB = () => {
+  rgb.value = `rgb(${red.value}, ${green.value}, ${blue.value})`;
 };
 
 sliders.forEach((slider) => {
   slider.addEventListener('input', (event) => {
     updateBoxRGB();
+    updateHex();
+    updateRGB();
     //console.log("Test");
     //console.log(event);
   });
