60422c2fc284dcdaca3921dec9d74894f6b22c5c
Robert King <robert.o.king@hotmail.co.uk>
2020-04-07 13:46:25 +0200

    Hue calculation from RGB; some commenting

--- colour_translator/main.js
+++ colour_translator/main.js
@@ -1,5 +1,7 @@
+// The sample colour region in the middle of the page
 const box = document.getElementById('box');
 
+// Get all our inputs via DOM
 const slidersRGB = document.querySelectorAll('.rgb input');
 const red = document.getElementById('red');
 const green = document.getElementById('green');
@@ -14,27 +16,36 @@ const hue = document.getElementById('hue');
 const saturation = document.getElementById('saturation');
 const lightness = document.getElementById('lightness');
 
+// Use RGB sliders' values to update colour sample region
 const updateBoxColour = () => {
   box.style.backgroundColor = `rgb(${red.value}, ${green.value}, ${blue.value})`;
 };
 
+// Use RGB sliders' values to update hex code in text input
 const updateHex = () => {
+  // First calculate the individual components of the hex colour
+  // Convert the red value from base 10 to hex (uppercase)
   let hexRed = parseInt(red.value).toString(16).toUpperCase();
+  // We need to append 0 before a one digit value
   if (hexRed.length === 1) {
     hexRed = "0" + hexRed;
   }
+
   let hexGreen = parseInt(green.value).toString(16).toUpperCase();
   if (hexGreen.length === 1) {
     hexGreen = "0" + hexGreen;
   }
+
   let hexBlue = parseInt(blue.value).toString(16).toUpperCase();
   if (hexBlue.length === 1) {
     hexBlue = "0" + hexBlue;
   }
 
+  // Update the hex colour based on the values calculated
   hex.value = `#${hexRed}${hexGreen}${hexBlue}`;
 };
 
+// Use RGB sliders' values to update RGB code in text input
 const updateRGB = () => {
   rgb.value = `rgb(${red.value}, ${green.value}, ${blue.value})`;
 };
@@ -48,7 +59,19 @@ const calculateS = (delta, lValue) => {
 };
 
 const calculateH = (rPrime, gPrime, bPrime, cMax, cMin, delta) => {
-
+  let hValue;
+  if (delta == 0) {
+    hValue = 0;
+  } else if (cMax === rPrime) {
+    hValue = 60 * (((gPrime - bPrime) / delta) % 6);
+  } else if (cMax === gPrime) {
+    hValue = 60 * (((bPrime - rPrime) / delta) + 2);
+  } else if (cMax === bPrime) {
+    hValue = 60 * (((rPrime - gPrime) / delta) + 4);
+  } else {
+    hValue = "Oops";
+  }
+  return hValue;
 };
 
 const updateHSL = () => {
@@ -59,10 +82,17 @@ const updateHSL = () => {
   const cMin = Math.min(rPrime, gPrime, bPrime);
   const delta = cMax - cMin;
 
-  // const hValue = calculateH(rPrime, gPrime, bPrime, cMax, cMin, delta);
-  const lValue = (cMax + cMin) / 2;
-  const sValue = calculateS(delta, lValue);
-  console.log(lValue);
+  const hValue = calculateH(rPrime, gPrime, bPrime, cMax, cMin, delta);
+  const lRaw = (cMax + cMin) / 2;
+  const lValue = lRaw * 100;
+  const sRaw = calculateS(delta, lValue);
+  const sValue = sRaw * 100;
+
+  hue.value = hValue;
+  lightness.value = lValue;
+  saturation.value = sValue;
+
+  hsl.value = `hsl(${hue.value}, ${lightness.value}%, ${saturation.value}%)`;
 };
 
 slidersRGB.forEach((slider) => {
