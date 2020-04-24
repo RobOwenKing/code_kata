b52f8457bed1d214039792aa16dc9d423aecf912
Robert King <robert.o.king@hotmail.co.uk>
2020-04-24 11:22:06 +0200

    caesar method improved to pass all 4 tests

--- string_methods/string_methods.rb
+++ string_methods/string_methods.rb
@@ -17,7 +17,7 @@ class String
   # caesar shift
   def caesar(offset = 1)
     a_to_z = ('a'..'z').to_a
-    offset_alphabet = a_to_z.slice!(offset..25) + a_to_z
+    offset_alphabet = a_to_z.slice!((offset % 26)...a_to_z.length) + a_to_z
     alphabet_string = offset_alphabet.join
     self.tr('A-Za-z', alphabet_string.upcase + alphabet_string)
   end
