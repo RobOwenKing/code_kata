f78ddb4f42140a740cd4de4211d82192bd6e47f7
Robert King <robert.o.king@hotmail.co.uk>
2020-04-26 00:18:08 +0200

    Refactoring

--- string_methods/calculate.rb
+++ string_methods/calculate.rb
@@ -1,23 +1,25 @@
 require_relative 'string_methods'
 
+def eval_expr(expression)
+  value = self.match(/\A\d+/)[0].to_i
+  # expression = self.delete_prefix(value.to_s)
+end
+
+def eval_list(expression)
+  terms = expression.slice(1..-1).map!(&:to_i)
+  terms.reduce(expression[0].to_sym)
+  # case self[0]
+  # when '+' then list.reduce(:+)
+  # when '-' then list.reduce(:-)
+  # when '*' then list.reduce(:*)
+  # when '/' then list.reduce(:/)
+  # end
+end
+
 class String
-  def eval_expr
-    value = self.match(/\A\d+/)[0].to_i
-    # expression = self.delete_prefix(value.to_s)
-  end
-
-  def eval_list
-    list = self.split.slice(1..-1).map!(&:to_i)
-    case self[0]
-    when '+' then list.reduce(:+)
-    when '-' then list.reduce(:-)
-    when '*' then list.reduce(:*)
-    when '/' then list.reduce(:/)
-    end
-  end
-
   def calculate
     operators = ['+', '-', '*', '/']
-    operators.include?(self[0]) ? self.eval_list : self.eval_expr
+    expression = self.split
+    operators.include?(expression[0]) ? eval_list(expression) : eval_expr(expression)
   end
 end
