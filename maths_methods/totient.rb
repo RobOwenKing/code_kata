a6ad9d8118726d704f0ae5efc2d5248d9f95e680
Robert King <robert.o.king@hotmail.co.uk>
2020-05-04 17:42:02 +0200

    More totient examples

--- maths_methods/totient.rb
+++ maths_methods/totient.rb
@@ -3,7 +3,7 @@ def coprime?(a, b)
 end
 
 def totient(n)
-  (0..n).to_a.count { |num| coprime?(n,num) }
+  (0..n).to_a.count { |num| coprime?(n, num) }
 end
 
 # puts coprime?(3, 6)
@@ -13,3 +13,7 @@ end
 puts totient(9)  # Should be 6
 puts totient(12) # Should be 4
 puts totient(13) # Should be 12
+# Speed tests
+puts totient(1_001)   # Should be 720
+puts totient(10_001)  # Should be 9792
+puts totient(100_001) # Should be 90900
