b839e7bbb7ba81d97f55aedd4a315311937ba0fd
Robert King <robert.o.king@hotmail.co.uk>
2020-10-21 12:00:42 +0200

    Added: Tests for #length method

--- data_structures/set_spec.rb
+++ data_structures/set_spec.rb
@@ -2,7 +2,20 @@ require 'rspec'
 require_relative 'set'
 
 RSpec.describe Set do
-  describe '' do
-
+  describe '#initialize' do
+    it 'Creates a set object' do
+      expect(Set.new.class).to eql(Set)
+    end
+  end
+  describe '#length' do
+    it 'Returns 0 for empty set' do
+      expect(Set.new.length).to eql(0)
+    end
+    it 'Returns correct number for set created with array of unique values' do
+      expect(Set.new([0, 1, 2, 3]).length).to eql(4)
+    end
+    it 'Returns correct number for set created with array of non-unique values' do
+      expect(Set.new([0, 1, 1, 2]).length).to eql(3)
+    end
   end
 end
