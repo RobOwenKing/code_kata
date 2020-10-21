2100ee1df9980e552fbe8e93497bd56c1587c268
Robert King <robert.o.king@hotmail.co.uk>
2020-10-21 11:54:09 +0200

    Refactored: queue_spec to use Ruby Queue for checks

--- data_structures/queue_spec.rb
+++ data_structures/queue_spec.rb
@@ -3,25 +3,32 @@ require_relative 'queue'
 
 RSpec.describe MyQueue do
   describe '#enqueue and #dequeue' do
-    queue = MyQueue.new
+    my_queue = MyQueue.new
+    ruby_queue = Queue.new
     it 'Returns nil for empty queue' do
-      expect(queue.dequeue).to eql(nil)
+      expect(my_queue.dequeue).to eql(nil)
     end
     it 'Dequeues correct value after one enqueue' do
-      queue.enqueue(10)
-      expect(queue.dequeue).to eql(10)
+      my_queue.enqueue(10)
+      ruby_queue.enq(10)
+      expect(my_queue.dequeue).to eql(ruby_queue.deq)
     end
     it 'Dequeues correct value after two enqueues' do
-      queue.enqueue(20)
-      queue.enqueue(30)
-      expect(queue.dequeue).to eql(20)
+      my_queue.enqueue(20)
+      ruby_queue.enq(20)
+      my_queue.enqueue(30)
+      ruby_queue.enq(30)
+      expect(my_queue.dequeue).to eql(ruby_queue.deq)
     end
     it 'Dequeues correct value second time' do
       # Queue still contains 30 from previous test
-      queue.enqueue(40)
-      queue.enqueue(50)
-      queue.dequeue
-      expect(queue.dequeue).to eql(40)
+      my_queue.enqueue(40)
+      ruby_queue.enq(40)
+      my_queue.enqueue(50)
+      ruby_queue.enq(50)
+      my_queue.dequeue
+      ruby_queue.deq
+      expect(my_queue.dequeue).to eql(ruby_queue.deq)
     end
   end
 end
