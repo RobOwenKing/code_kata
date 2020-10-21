7320dcf2d41e608f4b390619ccbf1d0641bd4f88
Robert King <robert.o.king@hotmail.co.uk>
2020-10-21 10:24:52 +0200

    Added: #dequeue to pass first test

--- data_structures/queue.rb
+++ data_structures/queue.rb
@@ -2,3 +2,14 @@
 # Inspired by InterviewNest (https://www.youtube.com/watch?v=esuEG9VINVA)
 
 require_relative 'stack'
+
+class Queue
+  def initialize
+    @for_enqueue = Stack.new
+    @for_dequeue = Stack.new
+  end
+
+  def dequeue
+    @for_dequeue.pop
+  end
+end
--- data_structures/queue_spec.rb
+++ data_structures/queue_spec.rb
@@ -2,25 +2,25 @@ require 'rspec'
 require_relative 'queue'
 
 RSpec.describe Queue do
-  describe '#enque and #deque' do
+  describe '#enqueue and #dequeue' do
     queue = Queue.new
     it 'Returns nil for empty queue' do
-      expect(queue.deque).to eql(nil)
+      expect(queue.dequeue).to eql(nil)
     end
     it 'Returns the correct value after one round' do
-      queue.enque(10)
-      expect(queue.deque).to eql(10)
+      queue.enqueue(10)
+      expect(queue.dequeue).to eql(10)
     end
     it 'Returns the correct value after two rounds' do
-      queue.enque(20)
-      queue.enque(30)
-      expect(queue.deque).to eql(30)
+      queue.enqueue(20)
+      queue.enqueue(30)
+      expect(queue.dequeue).to eql(30)
     end
     it 'Returns the correct value second time' do
-      queue.enque(40)
-      queue.enque(50)
-      queue.deque
-      expect(queue.deque).to eql(40)
+      queue.enqueue(40)
+      queue.enqueue(50)
+      queue.dequeue
+      expect(queue.dequeue).to eql(40)
     end
   end
 end
