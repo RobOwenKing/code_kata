1ddac56c47f7df56547d26d6cd876ae7c43ab3ed
Robert King <robert.o.king@hotmail.co.uk>
2020-10-21 14:14:04 +0200

    Added: .sort when initializing Set

--- data_structures/set.rb
+++ data_structures/set.rb
@@ -1,6 +1,12 @@
+# Note, Ruby's standard libraries include a Set class
+# Also, this is built using Arrays as an exercise
+# In a real use case, a Hash would likely be preferable
+
 class Set
   def initialize(array = [])
-    @set = array.uniq
+    # We want only unique values in our array
+    # Sorting it now will be useful later
+    @set = array.uniq.sort
   end
 
   def length
