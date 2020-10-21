de764dbc5552484478ba32b4b0676530c7649b43
Robert King <robert.o.king@hotmail.co.uk>
2020-10-21 15:45:42 +0200

    Started: Priority Queue spec and class copying Queue

---
+++ data_structures/priority_queue.rb
@@ -0,0 +1,23 @@
+# Queue implemented using two stacks
+# Inspired by InterviewNest (https://www.youtube.com/watch?v=esuEG9VINVA)
+
+require_relative 'priority_queue'
+
+class MyQueue
+  def initialize
+    @for_enqueue = Stack.new
+    @for_dequeue = Stack.new
+  end
+
+  def enqueue(val)
+    @for_enqueue.push(val)
+  end
+
+  def dequeue
+    if @for_dequeue.empty?
+      @for_dequeue.push(@for_enqueue.pop) until @for_enqueue.empty?
+    end
+
+    @for_dequeue.pop
+  end
+end
---
+++ data_structures/priority_queue_spec.rb
@@ -0,0 +1,34 @@
+require 'rspec'
+require_relative 'queue'
+
+RSpec.describe MyQueue do
+  describe '#enqueue and #dequeue' do
+    my_queue = MyQueue.new
+    ruby_queue = Queue.new
+    it 'Returns nil for empty queue' do
+      expect(my_queue.dequeue).to eql(nil)
+    end
+    it 'Dequeues correct value after one enqueue' do
+      my_queue.enqueue(10)
+      ruby_queue.enq(10)
+      expect(my_queue.dequeue).to eql(ruby_queue.deq)
+    end
+    it 'Dequeues correct value after two enqueues' do
+      my_queue.enqueue(20)
+      ruby_queue.enq(20)
+      my_queue.enqueue(30)
+      ruby_queue.enq(30)
+      expect(my_queue.dequeue).to eql(ruby_queue.deq)
+    end
+    it 'Dequeues correct value second time' do
+      # Queue still contains 30 from previous test
+      my_queue.enqueue(40)
+      ruby_queue.enq(40)
+      my_queue.enqueue(50)
+      ruby_queue.enq(50)
+      my_queue.dequeue
+      ruby_queue.deq
+      expect(my_queue.dequeue).to eql(ruby_queue.deq)
+    end
+  end
+end
