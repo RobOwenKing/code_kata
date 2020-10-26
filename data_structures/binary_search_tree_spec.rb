a5ab98a16588a1146e83909e11c2aad96c4c231a
Robert King <robert.o.king@hotmail.co.uk>
2020-10-26 17:12:59 +0100

    Added: #insert tests

--- data_structures/binary_search_tree_spec.rb
+++ data_structures/binary_search_tree_spec.rb
@@ -3,6 +3,24 @@ require_relative 'binary_search_tree'
 
 RSpec.describe BinarySearchTree do
   describe '#insert' do
-
+    insert_tree = BinarySearchTree.new(10)
+    insert_root = insert_tree.root
+    it 'should give root node a left child when passed a smaller value' do
+      insert_tree.insert(1)
+      expect(insert_root.left.value).to eql(1)
+    end
+    it 'should give root node a right child when passed a larger value' do
+      insert_tree.insert(100)
+      expect(insert_root.right.value).to eql(1)
+    end
+    it 'should place values in the correct place further down the tree' do
+      insert_tree.insert(5)
+      expect(insert_root.left.right.value).to eql(5)
+    end
+    it 'should not add repeat values to the tree' do
+      insert_tree.insert(100)
+      expect(insert_root.right.left).to eql(nil)
+      expect(insert_root.right.right).to eql(nil)
+    end
   end
 end
