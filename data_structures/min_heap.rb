4210d9703ac205f244ad728d7d697e00e8e59e9c
Robert King <robert.o.king@hotmail.co.uk>
2020-12-07 18:26:37 +0100

    Commented: #bubble_up and #min_child_index

--- data_structures/min_heap.rb
+++ data_structures/min_heap.rb
@@ -73,31 +73,44 @@ class MinHeap
   # If a swap has taken place, recursively bubble
   def bubble_up(index)
     parent = parent_index(index)
+    # If the parent's value is smaller than the child, do nothing
     return unless @heap[parent] > @heap[index]
 
+    # Else, swap the child with its parent
     swap(index, parent)
+    # Then iterate #bubble_up
     bubble_up(parent) unless parent.zero?
   end
 
+  # Input: Integer, the index of an element in @heap
   def min_child_index(index)
     left_child_index = left_child_index(index)
+    # Return nil if it has no left child
     return nil if left_child_index.nil?
 
     right_child_index = right_child_index(index)
+    # If it has only a left child, return that
     return left_child_index if right_child_index.nil?
 
+    # If it has two children, return the index of the child with smaller value
     @heap[left_child_index] < @heap[right_child_index] ? left_child_index : right_child_index
   end
 
+  # Input: Integer, the index of the element to start with
   def bubble_down(index)
+    # We first need the index of the lesser of its two children
     min_child_index = min_child_index(index)
+    # If it has no children, do nothing
     return if min_child_index.nil?
 
+    # If it is less than its min child, do nothing
     current = @heap[index]
     min_child = @heap[min_child_index]
     return unless current > min_child
 
+    # Otherwise, we need to swap the element with its smaller child
     swap(index, min_child_index)
+    # Then iterate #bubble_down
     bubble_down(min_child_index)
   end
 end
