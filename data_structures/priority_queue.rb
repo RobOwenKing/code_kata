3b3bbef3a19897ab705f1718520fa5ebf912b651
Robert King <robert.o.king@hotmail.co.uk>
2020-12-08 16:51:34 +0100

    Added: PriorityQueue comments and Contents

--- data_structures/priority_queue.rb
+++ data_structures/priority_queue.rb
@@ -1,10 +1,22 @@
-# Priority Queue class with some basic operations
+# A Priority Queue class in Ruby with some basic operations
+# Contents
+# - class PriorityQueue
+# - - #initialize
+# - - #enqueue(val, priority)
+# - - #dequeue
+# - - #next
+# - - #find_priority(value)
+# - - #position(value)
+# - - #change_priority(value, priority)
+
 class PriorityQueue
   def initialize
     # I'll use an array to store the elements
+    # Each entry will itself be an array [val, priority]
     @queue = []
   end
 
+  # Add the given value to our queue with given priority
   def enqueue(val, priority)
     # Enter elements ordered by priority so #dequeue is O(1)
     # Find the position of the first element with a higher priority
@@ -15,16 +27,19 @@ class PriorityQueue
     pos.nil? ? @queue.push([val, priority]) : @queue.insert(pos, [val, priority])
   end
 
+  # Delete the highest priority element and returns its value
   def dequeue
-    # Return the value of the last element, else nil
+    # Return the value (first element) of the last element of the queue, else nil
     @queue.empty? ? nil : @queue.pop[0]
   end
 
   # A #peek method
+  # Returns the value of the next element without deleting it
   def next
     @queue.empty? ? nil : @queue[-1][0]
   end
 
+  # Find the priority associated with the given value
   # Not worrying about uniqueness
   def find_priority(value)
     # Find an item in the queue whose first element matches given value
@@ -33,6 +48,7 @@ class PriorityQueue
     found.nil? ? nil : found[1]
   end
 
+  # Return a value's position in the queue (highest priority value returns 1)
   # Again, not worrying about uniqueness
   def position(value)
     # Find the index of some element with given value
@@ -42,6 +58,7 @@ class PriorityQueue
   end
 
   # Still not worrying about uniqueness
+  # We fake this by deleting the old element and creating a new one
   def change_priority(value, priority)
     # Plain #delete would delete any that match value
     index = @queue.find_index { |item| item[0] == value }
