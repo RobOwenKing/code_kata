0c440f7c2a81d53cf7f07ec875db356a751aeec9
Robert King <robert.o.king@hotmail.co.uk>
2022-03-06 17:36:04 +0000

    fix: #deep_equals? works when arr1.length < arr2.length but all elements match until end

--- array_methods/array_methods.rb
+++ array_methods/array_methods.rb
@@ -2,6 +2,8 @@
 
 # Returns a boolean value, whether the arrays have same element in same order
 def deep_equals?(arr1, arr2)
+  return false if arr1.length != arr2.length
+
   arr1.each_with_index do |x, i|
     return false if x.instance_of?(Array) && !deep_equals?(x, arr2[i])
     return false if x != arr2[i]
