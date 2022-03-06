42c9dd2dca1a6f97a55a6b07af0b2c2090c9ff5b
Robert King <robert.o.king@hotmail.co.uk>
2022-03-06 17:35:11 +0000

    feat: deep_equals? with spec (one failure)

--- array_methods/array_methods.rb
+++ array_methods/array_methods.rb
@@ -1,3 +1,18 @@
+# frozen_string_literal: true
+
+# Returns a boolean value, whether the arrays have same element in same order
+def deep_equals?(arr1, arr2)
+  arr1.each_with_index do |x, i|
+    return false if x.instance_of?(Array) && !deep_equals?(x, arr2[i])
+    return false if x != arr2[i]
+  end
+
+  true
+end
+
+# Returns a new array with elements of arr1 not in arr2
+# Compare: [1, 2, 2] - [1, 2] #=> []
+#          subtract([1, 2, 2], [1, 2]) #=> [2]
 def subtract(arr1, arr2)
   arr1
 end
--- array_methods/array_methods_spec.rb
+++ array_methods/array_methods_spec.rb
@@ -3,6 +3,26 @@
 require 'rspec'
 require_relative 'array_methods'
 
-RSpec.describe '#subtract' do
-
+RSpec.describe '#deep_equals?' do
+  it 'should return true when true' do
+    expect(deep_equals?([1, 2, 3], [1, 2, 3])).to be true
+  end
+  it 'should return true when nested arrays match too' do
+    expect(deep_equals?([1, 2, [1, 2]], [1, 2, [1, 2]])).to be true
+  end
+  it 'should return false when false' do
+    expect(deep_equals?([1, 2, 3], [4, 5, 6])).to be false
+  end
+  it 'should return false when nested arrays do not match' do
+    expect(deep_equals?([1, 2, [1, 2]], [1, 2, [1, 3]])).to be false
+  end
+  it 'should return false when same elements, not in the same order' do
+    expect(deep_equals?([1, 2, 3], [1, 3, 2])).to be false
+  end
+  it 'should return false when arr1 a subset of arr2' do
+    expect(deep_equals?([1, 2], [1, 2, 3])).to be false
+  end
+  it 'should return false when arr2 a subset of arr1' do
+    expect(deep_equals?([1, 2, 3], [1, 2])).to be false
+  end
 end
