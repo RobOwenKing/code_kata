b208b7e6a500db6f5f5b5c324f2e669a6542e55d
Robert King <robert.o.king@hotmail.co.uk>
2022-04-25 23:47:26 +0100

    test: Add many pending tests to sketch out various other basic methods

--- data_structures/graph_spec.rb
+++ data_structures/graph_spec.rb
@@ -11,12 +11,14 @@ RSpec.describe Graph do
 
     it 'gives new Graph the expected default properties' do
       graph = Graph.new
+
       expect(graph.directed).to eql(false)
       expect(graph.weighted).to eql(false)
     end
 
     it 'gives new Graph the expected passed properties' do
       graph = Graph.new(directed: true, weighted: true)
+
       expect(graph.directed).to eql(true)
       expect(graph.weighted).to eql(true)
     end
@@ -25,6 +27,7 @@ RSpec.describe Graph do
   describe '#add_vertex' do
     it 'returns expected values' do
       graph = Graph.new
+
       expect(graph.add_vertex(1)).to eql(1)
       expect(graph.add_vertex('a')).to eql('a')
       expect(graph.add_vertex(1)).to eql(nil)
@@ -34,10 +37,40 @@ RSpec.describe Graph do
   describe '#delete_vertex' do
     it 'returns expected values' do
       graph = Graph.new
+
       expect(graph.add_vertex(1)).to eql(1)
       expect(graph.delete_vertex(1)).to eql(1)
       expect(graph.delete_vertex('a')).to eql(nil)
       expect(graph.delete_vertex(1)).to eql(nil)
     end
+    it 'deletes all adjacent edges'
+  end
+
+  describe '#add_edge' do
+    it 'adds to both vertices in an undirected graph'
+    it 'adds to only one vertex in a directed graph'
+    it 'returns nil if one or both params does not exist'
+  end
+
+  describe '#delete_edge' do
+    it 'works correctly in an undirected graph'
+    it 'works correctly in a directed graph'
+    it 'returns nil if one or both params does not exist'
+  end
+
+  describe '#vertices' do
+    it 'works correctly for an empty graph'
+    it 'works correctly for a larger graph'
+  end
+
+  describe '#edges' do
+    it 'works correctly for an empty graph'
+    it 'works correctly for a larger graph'
+  end
+
+  describes '#neighbours' do
+    it 'works correctly for an isolated vertex'
+    it 'works correctly for non-isolated vertices'
+    it 'returns nil if no such vertex exists'
   end
 end
