3770650e878fad923626fd921b965454eb98f047
Robert King <robert.o.king@hotmail.co.uk>
2022-04-26 17:08:08 +0100

    test: Add for #add_edge, #delete_edge and more for #delete_vertex

--- data_structures/graph_spec.rb
+++ data_structures/graph_spec.rb
@@ -13,70 +13,164 @@ RSpec.describe Graph do
     end
 
     it 'gives new Graph the expected default properties' do
-      graph = Graph.new
+      @graph = Graph.new
 
-      expect(graph.directed).to eql(false)
-      expect(graph.weighted).to eql(false)
+      expect(@graph.directed).to eql(false)
+      expect(@graph.weighted).to eql(false)
     end
 
     it 'gives new Graph the expected passed properties' do
-      graph = Graph.new(directed: true, weighted: true)
+      @graph = Graph.new(directed: true, weighted: true)
 
-      expect(graph.directed).to eql(true)
-      expect(graph.weighted).to eql(true)
+      expect(@graph.directed).to eql(true)
+      expect(@graph.weighted).to eql(true)
     end
   end
 
   describe '#add_vertex' do
     it 'returns expected values' do
-      graph = Graph.new
+      @graph = Graph.new
 
-      expect(graph.add_vertex(1)).to eql(1)
-      expect(graph.add_vertex('a')).to eql('a')
-      expect(graph.add_vertex(1)).to eql(nil)
+      expect(@graph.add_vertex(1)).to eql(1)
+      expect(@graph.add_vertex('a')).to eql('a')
+      expect(@graph.add_vertex(1)).to eql(nil)
     end
   end
 
   describe '#delete_vertex' do
     it 'returns expected values' do
-      graph = Graph.new
+      @graph = Graph.new
 
-      expect(graph.add_vertex(1)).to eql(1)
-      expect(graph.delete_vertex(1)).to eql(1)
-      expect(graph.delete_vertex('a')).to eql(nil)
-      expect(graph.delete_vertex(1)).to eql(nil)
+      expect(@graph.add_vertex(1)).to eql(1)
+      expect(@graph.delete_vertex(1)).to eql(1)
+      expect(@graph.delete_vertex('a')).to eql(nil)
+      expect(@graph.delete_vertex(1)).to eql(nil)
     end
-    it 'deletes all adjacent edges'
+
+    it 'deletes the given vertex' do
+      @graph = Graph.new
+
+      @graph.add_vertex(1)
+      @graph.add_vertex(2)
+      @graph.delete_vertex(1)
+
+      expect(@graph.vertices.include?(2)).to eq(true)
+      expect(@graph.vertices.include?(1)).to eq(false)
+    end
+
+    it 'deletes all adjacent edges in an undirected graph' do
+      @graph = Graph.new
+
+      @graph.add_vertex(1)
+      @graph.add_vertex(2)
+      @graph.add_vertex(3)
+
+      @graph.add_edge(1, 2)
+      @graph.add_edge(1, 3)
+
+      @graph.delete_vertex(2)
+
+      expect(deep_equals?(@graph.neighbours(1), [3])).to eq(true)
+      expect(deep_equals?(@graph.neighbours(3), [1])).to eq(true)
+    end
   end
 
   describe '#add_edge' do
-    it 'adds to both vertices in an undirected graph'
-    it 'adds to only one vertex in a directed graph'
-    it 'returns nil if one or both params does not exist'
+    it 'adds to both vertices in an undirected graph' do
+      @graph = Graph.new
+
+      @graph.add_vertex(1)
+      @graph.add_vertex(2)
+
+      @graph.add_edge(1, 2)
+
+      expect(@graph.neighbours(1).include?(2)).to eq(true)
+      expect(@graph.neighbours(2).include?(1)).to eq(true)
+    end
+
+    it 'adds to only one vertex in a directed graph' do
+      @graph = Graph.new(directed: true)
+
+      @graph.add_vertex(1)
+      @graph.add_vertex(2)
+
+      @graph.add_edge(1, 2)
+
+      expect(@graph.neighbours(1).include?(2)).to eq(true)
+      expect(@graph.neighbours(2).include?(1)).to eq(false)
+    end
+
+    it 'returns nil if one or both params does not exist' do
+      @graph = Graph.new
+
+      @graph.add_vertex(1)
+      @graph.add_vertex(2)
+
+      expect(@graph.add_edge(1, 3)).to eq(nil)
+      expect(@graph.add_edge(4, 2)).to eq(nil)
+    end
   end
 
   describe '#delete_edge' do
-    it 'works correctly in an undirected graph'
-    it 'works correctly in a directed graph'
-    it 'returns nil if one or both params does not exist'
+    it 'works correctly in an undirected graph' do
+      @graph = Graph.new
+
+      @graph.add_vertex(1)
+      @graph.add_vertex(2)
+
+      @graph.add_edge(1, 2)
+      @graph.delete_edge(2, 1)
+
+      expect(@graph.neighbours(1).include?(2)).to eq(false)
+      expect(@graph.neighbours(2).include?(1)).to eq(false)
+    end
+
+    it 'works correctly in a directed graph' do
+      @graph = Graph.new(directed: true)
+
+      @graph.add_vertex(1)
+      @graph.add_vertex(2)
+
+      @graph.add_edge(1, 2)
+      @graph.delete_edge(2, 1)
+
+      expect(@graph.neighbours(1).include?(2)).to eq(true)
+      expect(@graph.neighbours(2).include?(1)).to eq(false)
+
+      @graph.delete_edge(1, 2)
+
+      expect(@graph.neighbours(1).include?(2)).to eq(false)
+      expect(@graph.neighbours(2).include?(1)).to eq(false)
+    end
+
+    it 'returns nil if one or both params does not exist' do
+      @graph = Graph.new
+
+      @graph.add_vertex(1)
+      @graph.add_vertex(2)
+
+      expect(@graph.delete_edge(1, 3)).to eq(nil)
+      expect(@graph.delete_edge(4, 2)).to eq(nil)
+    end
   end
 
   describe '#vertices' do
     it 'works correctly for an empty graph' do
-      graph = Graph.new
+      @graph = Graph.new
 
-      expect(graph.vertices.class).to eq(Array)
-      expect(graph.vertices.length).to eq(0)
+      expect(@graph.vertices.class).to eq(Array)
+      expect(@graph.vertices.length).to eq(0)
     end
+
     it 'works correctly for a larger graph' do
-      graph = Graph.new
+      @graph = Graph.new
 
-      graph.add_vertex(1)
-      graph.add_vertex(2)
-      graph.add_vertex(3)
-      graph.delete_vertex(2)
+      @graph.add_vertex(1)
+      @graph.add_vertex(2)
+      @graph.add_vertex(3)
+      @graph.delete_vertex(2)
 
-      expect(deep_equals?(graph.vertices, [1, 3])).to eq(true)
+      expect(deep_equals?(@graph.vertices, [1, 3])).to eq(true)
     end
   end
 
