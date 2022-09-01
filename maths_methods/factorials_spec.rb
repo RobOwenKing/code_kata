fcc2f4093d409207a9b4b6d742544f8f903a836a
Robert King <robert.o.king@hotmail.co.uk>
2022-09-01 12:24:21 +0100

    test: Add tests for intended use of #multifactorial

--- maths_methods/factorials_spec.rb
+++ maths_methods/factorials_spec.rb
@@ -43,3 +43,33 @@ RSpec.describe '#semifactorial' do
     expect(semifactorial(9)*semifactorial(10)).to be(factorial(10))
   end
 end
+
+RSpec.describe '#multifactorial' do
+  it 'should return 1 when passed start 0' do
+    expect(multifactorial(0, 1).to be(1))
+    expect(multifactorial(0, 2).to be(1))
+  end
+  it 'should be the same as #factorial when step = 1' do
+    expect(multifactorial(1, 1)).to be(factorial(1))
+    expect(multifactorial(2, 1)).to be(factorial(2))
+    expect(multifactorial(4, 1)).to be(factorial(4))
+    expect(multifactorial(9, 1)).to be(factorial(9))
+  end
+  it 'should be the same as #semifactorial when step = 2' do
+    expect(multifactorial(1, 2)).to be(semifactorial(1))
+    expect(multifactorial(2, 2)).to be(semifactorial(2))
+    expect(multifactorial(4, 2)).to be(semifactorial(4))
+    expect(multifactorial(9, 2)).to be(semifactorial(9))
+  end
+  it 'should work for higher values of step' do
+    expect(multifactorial(6, 3)).to be(18)
+    expect(multifactorial(6, 4)).to be(12)
+    expect(multifactorial(6, 5)).to be(6)
+    expect(multifactorial(7, 3)).to be(28)
+    expect(multifactorial(7, 4)).to be(21)
+    expect(multifactorial(7, 5)).to be(14)
+    expect(multifactorial(10, 3)).to be(280)
+    expect(multifactorial(10, 4)).to be(120)
+    expect(multifactorial(10, 5)).to be(50)
+  end
+end
